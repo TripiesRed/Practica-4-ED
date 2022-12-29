934d846ea0989da6ca2900970f355b56d00766c4
TripiesRed <gonzalitoo2spam@gmail.com>
2022-12-29 18:34:16 +0100

    Ejer 4

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/diegoos_03/CLionProjects/Practica-4-ED
+# Install script for directory: /home/gonzalomp/Escritorio/EstructuraDatos/ej04_juego_letras
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/diegoos_03/CLionProjects/Practica-4-ED/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/gonzalomp/Escritorio/EstructuraDatos/ej04_juego_letras/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
